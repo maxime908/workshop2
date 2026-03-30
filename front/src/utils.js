9782d62feed01f762f79e2def8b698be1e1f377e
= <=>
2026-03-30 15:37:52 +0200

    Make post, patch, get

--- front/src/utils.js
+++ front/src/utils.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
-const url = "../../api/"
+const url = "http://localhost/workshop2/api/"
 
 export function getAPI(path) {
-    return axios.get(path)
+    return axios.get(url + path)
         .then(response => response)
         .catch(err => console.log(err));
 }
