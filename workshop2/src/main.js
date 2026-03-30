00c247b1ebf17e9701a35ab819e000e74217b343
Marc Legros <auflo2007@gmail.com>
2026-03-30 13:58:48 +0200

    init project

--- workshop2/src/main.js
+++ workshop2/src/main.js
@@ -2,6 +2,7 @@ import './style.css'
 import javascriptLogo from './assets/javascript.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import axios from "axios";
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
