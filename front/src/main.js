b320266425c7cf765cc7efac615fbffb97794411
= <=>
2026-03-31 14:42:53 +0200

    update SQL schema and modify main.js for device_id handling

--- front/src/main.js
+++ front/src/main.js
@@ -4,18 +4,3 @@ import { getAPI } from "./utils";
 const a = await getAPI("");
 
 console.log(a)
-
-axios.post("http://localhost/workshop2/api/lamarr/", {
-    device_id: 6577445544,
-})
-.then((val) => console.log(val))
-
-axios.post('http://localhost/workshop2/api/lamarr/', {
-    device_id: 5566554
-})
-.then(function (response) {
-console.log(response);
-})
-.catch(function (error) {
-console.log(error);
-});
