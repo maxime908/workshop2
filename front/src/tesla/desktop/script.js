204a45cb82d596a70a9408d8951e6fe0dd6c7cac
Gaspar <gaspar.deutsch@gmail.com>
2026-04-02 13:27:02 +0200

    :wrench: working on tesla version

--- front/src/tesla/desktop/script.js
+++ front/src/tesla/desktop/script.js
@@ -3,11 +3,11 @@ import { readJSONFile } from "../../utils";
 
 let oldData;
 
-
 // Tableau de mes vidéos
 const video = [
     "../assets/step1.mp4",
     "../assets/step2.mp4",
+    "../assets/336603.mp4",
     "../assets/step3.mp4"
 ]
 
@@ -26,43 +26,42 @@ function changeVideo(step) {
     }
 }
 
+// changeVideo(dataStep)
+
+
 
 async function changeWindow() {
 
     const data = await readJSONFile('/steps.json')
-    const dataStep = data.tesla.step
+    const dataStep = data.bass.step
 
     if (!oldData) {
         oldData = ""
         console.log("DataStep au tout début donne", dataStep)
-        hideStep(0)
-        showStep(dataStep)
+        // hideStep(0)
+        // showStep(dataStep)
     }
 
-    if (JSON.stringify(data.tesla) == JSON.stringify(oldData)) {
+    if (JSON.stringify(data.bass) == JSON.stringify(oldData)) {
         console.log("Aucune valeur changée")
     } else {
 
-        if (JSON.stringify(data.tesla.step) != JSON.stringify(oldData.step)) {
-            hideStep(dataStep - 1)
-            showStep(dataStep)
-            
-            changeVideo(dataStep)
+        if (JSON.stringify(data.bass.step) != JSON.stringify(oldData.step)) {
+            // hideStep(dataStep - 1)
+            // showStep(dataStep)
 
+            changeVideo(dataStep)
         } else {
             console.log("Le params a changé")
-            if (data.tesla.params == "goodAnswer") {
+            if (data.bass.params == "goodAnswer") {
                 showAnswer(dataStep, true)
-
-                changeVideo(dataStep)
-
-            } else if (data.tesla.params == "wrongAnswer") {
+            } else if (data.bass.params == "wrongAnswer") {
                 showAnswer(dataStep, false)
             }
         }
     }
 
-    oldData = data.tesla
+    oldData = data.bass
 }
 
 setInterval(changeWindow, 500)
