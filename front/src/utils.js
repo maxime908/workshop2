21b7d1b518aba27f5c5de263b39de530a78b4c84
forrobdev <robin.despaquis@gmail.com>
2026-04-02 11:13:41 +0200

    :sparkles: feat - added features to utils.js

--- front/src/utils.js
+++ front/src/utils.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
+//TODO : Change this to : /api/ for production 
 const url = "http://localhost/workshop2/api/"
 
 //Communications avec l'API
@@ -22,6 +23,14 @@ export function getStep(personality, step) {
         .catch(err => console.log(err));
 }
 
+export function getStats(device_id) {
+    return axios.post(url, {
+        device_id: device_id
+    })
+    .then(response => response)
+    .catch(err => console.log(err));
+}
+
 export async function createGame(personality) {
     let deviceId = getDeviceId()
     
@@ -31,14 +40,21 @@ export async function createGame(personality) {
         console.log("Nouveau device id :", deviceId)
     }
 
-    return axios.post(url + "/" + personality, {
-        deviceId: deviceId,
+    console.log("Je vais envoyer à",url + personality, "la valeur de device ID :",deviceId)
+    return axios.post(url + personality, {
+        device_id: deviceId,
+    })
+}
+
+export async function setParams(personality, params) {
+    return axios.post(url + personality + "/params", {
+        params: params,
     })
 }
 
 export async function updateGame(personality,deviceId, endDate, score) {
     return axios.patch(url + "/" + personality, {
-        deviceId: deviceId,
+        device_id: deviceId,
         endDate: endDate,
         score: score
     })
@@ -51,6 +67,25 @@ export async function updateScore(personality,score) {
     })
 }
 
+// Lire dans le fichier JSON (pour le desktop)
+export async function readJSONFile(file) {
+  try {
+
+    const response = await fetch(file);
+
+    if (!response.ok) {
+      throw new Error(`Erreur lors du chargement : ${response.status}`);
+    }
+
+    // On transforme la réponse en objet JavaScript utilisable
+    const data = await response.json();
+    return data;
+
+  } catch (error) {
+    console.error("Impossible de lire le fichier steps.json :", error);
+  }
+}
+
 
 
 // Gérer les deviceId
@@ -60,11 +95,8 @@ export function getDeviceId() {
 
 export function createDeviceId() {
     const deviceId = crypto.randomUUID();
+    // const deviceId = "TESTTESTTEST2456789"
     localStorage.setItem("deviceId",JSON.stringify(deviceId))
     
     return deviceId
 }
-
-
-
-
