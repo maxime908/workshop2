c8bd03becd82782d25e8b7a7a6cc8270c6404822
= <=>
2026-04-02 09:52:59 +0200

    commit main to production

--- api/config/config.php
+++ api/config/config.php
@@ -1,6 +1,6 @@
 <?php
     try {
-        $mysqlClient = new PDO('mysql:host=localhost;dbname=workshop2', 'root', '');
+        $mysqlClient = new PDO('mysql:host=83.228.195.160;port=3306;dbname=default', 'mysql', 'OcPCZUx8gG5ffRVPrZYONtnKWAY9iZJuH3KdBADPx0OKi04t74v1vbRnyBf8mi3V');
 
     $mysqlClient->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (Exception $exception) {
